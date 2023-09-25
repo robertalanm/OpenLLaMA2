b835e2247be45a5dcc45c7586231e7aee4d39413
Carro <robertmyers@hey.com>
2023-09-24 19:27:37 -0500

    a

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 ray job submit --address="http://127.0.0.1:8265" \
     --runtime-env-json='{"working_dir": "/home/ubuntu/ol2", "pip": "/home/ubuntu/ol2/requirements.txt"}' \
-    -- python3 examples/train_ppo_ray.py \
+    -- python3 examples/train_ray_ppo.py \
     --ref_num_nodes 1 \
     --ref_num_gpus_per_node 1 \
     --reward_num_nodes 1 \

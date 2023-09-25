5784bd516512687ee1647686ebf94a20037242b5
Carro <robertmyers@hey.com>
2023-09-24 19:25:33 -0500

    a

---
+++ train.sh
@@ -0,0 +1,35 @@
+ray job submit --address="http://127.0.0.1:8265" \
+    --runtime-env-json='{"working_dir": "/home/ubuntu/ol2", "pip": "/home/ubuntu/ol2/requirements.txt"}' \
+    -- python3 examples/train_ppo_ray.py \
+    --ref_num_nodes 1 \
+    --ref_num_gpus_per_node 1 \
+    --reward_num_nodes 1 \
+    --reward_num_gpus_per_node 1 \
+    --critic_num_nodes 1 \
+    --critic_num_gpus_per_node 2 \
+    --actor_num_nodes 1 \
+    --actor_num_gpus_per_node 4 \
+    --pretrain robertmyers/targon-7b \
+    --critic_pretrain robertmyers/targon-7b \
+    --model_revision v1.1.8 \
+    --save_path ~/ol2/ckpts \
+    --micro_train_batch_size 1 \
+    --train_batch_size 64 \
+    --micro_rollout_batch_size 1 \
+    --rollout_batch_size 1024 \
+    --max_epochs 1 \
+    --prompt_max_len 1024 \
+    --generate_max_len 1024 \
+    --zero_stage 2 \
+    --bf16 \
+    --actor_learning_rate 5e-7 \
+    --critic_learning_rate 9e-6 \
+    --inference_tp_size 1 \
+    --init_kl_coef 0.01 \
+    --prompt_data Open-Orca/OpenOrca,Dahoas/full-hh-rlhf,tasksource/oasst1_pairwise_rlhf_reward \
+    --prompt_data_probs 0.4,0.5,0.1 \
+    --max_samples 80000 \
+    --normalize_reward \
+    --actor_init_on_gpu \
+    --flash_attn \
+    --gradient_checkpointing
